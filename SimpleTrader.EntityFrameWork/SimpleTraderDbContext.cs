5bd1da5b751d88ac7f2f20ca968c0c3d94be9e57
Seiyon Lee <72716758+Seiyon@users.noreply.github.com>
2022-07-21 18:04:42 +0900

    초기 커밋.

---
+++ SimpleTrader.EntityFrameWork/SimpleTraderDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using SimpleTrader.Domain.Models;
+
+namespace SimpleTrader.EntityFrameWork
+{
+    class SimpleTraderDbContext : DbContext //store data
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<User>().Property(c => c.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
